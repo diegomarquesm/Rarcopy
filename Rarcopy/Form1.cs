ef82b801b09171a5b825dc5af5705c51f43ea576
diego.marques <diego.marqu3s@gmail.com>
2020-09-18 10:22:52 -0300

    Projeto Inicial

--- Rarcopy/Form1.cs
+++ Rarcopy/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Rarcopy
 {
@@ -15,6 +16,22 @@ namespace Rarcopy
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string startPath = @"c:\example\start";
+            string zipPath = @"c:\example\result.zip";
+            string extractPath = @"c:\example\extract";
+
+            System.IO.Compression.ZipFile.CreateFromDirectory(startPath, zipPath);
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
+
         }
     }
 }
---
+++ Rarcopy/System/IO/Compression/ZipFile.cs
@@ -0,0 +1,15 @@
+﻿namespace System.IO.Compression
+{
+    internal class ZipFile
+    {
+        internal static void CreateFromDirectory(string startPath, string zipPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void ExtractToDirectory(string zipPath, string extractPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
